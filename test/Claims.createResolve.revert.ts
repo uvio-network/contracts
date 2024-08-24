5a30c431b199e8cb9fb5127ce99453d18e318bfc
xh3b4sd <xh3b4sd@gmail.com>
2024-08-24 17:20:18 +0200

    add tests

--- test/Claims.createResolve.revert.ts
+++ test/Claims.createResolve.revert.ts
@@ -11,8 +11,8 @@ import { Index } from "./src/Index";
 describe("Claims", function () {
   describe("createResolve", function () {
     describe("revert", function () {
-      const createResolve = async () => {
-        const { Address, Balance, Claims, Signer, Token } = await loadFixture(Deploy);
+      const createPropose = async () => {
+        const { Balance, Claims, Signer } = await loadFixture(Deploy);
 
         await Balance([1, 2, 3, 4, 5], 10);
 
@@ -47,14 +47,20 @@ describe("Claims", function () {
           Expiry(2, "days"),
         );
 
+        return { Claims, Signer };
+      };
+
+      const expirePropose = async () => {
+        const { Claims, Signer } = await loadFixture(createPropose);
+
         await network.provider.send("evm_setNextBlockTimestamp", [Expiry(3, "days")]);
         await network.provider.send("evm_mine");
 
-        return { Address, Claims, Signer, Token };
-      }
+        return { Claims, Signer };
+      };
 
       it("if owner is signer", async function () {
-        const { Claims, Signer } = await loadFixture(createResolve);
+        const { Claims, Signer } = await loadFixture(expirePropose);
 
         const txn = Claims.connect(Signer(0)).createResolve(
           Claim(1),
@@ -67,7 +73,7 @@ describe("Claims", function () {
       });
 
       it("if voter is signer", async function () {
-        const { Claims, Signer } = await loadFixture(createResolve);
+        const { Claims, Signer } = await loadFixture(expirePropose);
 
         const txn = Claims.connect(Signer(1)).createResolve(
           Claim(1),
@@ -80,7 +86,7 @@ describe("Claims", function () {
       });
 
       it("if staker is signer", async function () {
-        const { Claims, Signer } = await loadFixture(createResolve);
+        const { Claims, Signer } = await loadFixture(expirePropose);
 
         const txn = Claims.connect(Signer(2)).createResolve(
           Claim(1),
@@ -90,6 +96,68 @@ describe("Claims", function () {
         );
 
         await expect(txn).to.be.revertedWithCustomError(Claims, "AccessControlUnauthorizedAccount");
+      });
+
+      it("if resolve already created, immediately", async function () {
+        const { Claims, Signer } = await loadFixture(expirePropose);
+
+        await Claims.connect(Signer(9)).createResolve(
+          Claim(1),
+          Claim(7),
+          [Index(0), Index(4)], // index 0 and 4 are address 1 and 5
+          Expiry(7, "days"),
+        );
+
+        const txn = Claims.connect(Signer(9)).createResolve(
+          Claim(1),
+          Claim(7),
+          [Index(0), Index(4)], // index 0 and 4 are address 1 and 5
+          Expiry(7, "days"),
+        );
+
+        await expect(txn).to.be.revertedWithCustomError(Claims, "Expired");
+      });
+
+      it("if resolve already created, later", async function () {
+        const { Claims, Signer } = await loadFixture(expirePropose);
+
+        await Claims.connect(Signer(9)).createResolve(
+          Claim(1),
+          Claim(7),
+          [Index(0), Index(4)], // index 0 and 4 are address 1 and 5
+          Expiry(7, "days"),
+        );
+
+        await network.provider.send("evm_setNextBlockTimestamp", [Expiry(8, "days")]);
+        await network.provider.send("evm_mine");
+
+        const txn = Claims.connect(Signer(9)).createResolve(
+          Claim(1),
+          Claim(7),
+          [Index(0), Index(4)], // index 0 and 4 are address 1 and 5
+          Expiry(7, "days"),
+        );
+
+        await expect(txn).to.be.revertedWithCustomError(Claims, "Expired");
+      });
+
+      it("if propose still active", async function () {
+        const { Claims, Signer } = await loadFixture(createPropose);
+
+        // Note that we are not expiring the propose in this test. We only move
+        // 5 minutes forward and try to create a resolve before the propose
+        // expired.
+        await network.provider.send("evm_setNextBlockTimestamp", [Expiry(5, "minutes")]);
+        await network.provider.send("evm_mine");
+
+        const txn = Claims.connect(Signer(9)).createResolve(
+          Claim(1),
+          Claim(7),
+          [Index(0), Index(4)], // index 0 and 4 are address 1 and 5
+          Expiry(7, "days"),
+        );
+
+        await expect(txn).to.be.revertedWithCustomError(Claims, "Expired");
       });
     });
   });
