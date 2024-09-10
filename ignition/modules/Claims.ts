b09fa258e54190f701f75f72aa2247f8648eb729
xh3b4sd <xh3b4sd@gmail.com>
2024-09-10 20:24:38 +0200

    update ignotion modules

--- ignition/modules/Claims.ts
+++ ignition/modules/Claims.ts
@@ -4,7 +4,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 //     npx hardhat ignition deploy ./ignition/modules/Claims.ts --network localhost
 //
 const Module = buildModule("Claims", (m) => {
-  return { Claims: m.contract("Claims", ["0xEFb36B2D443C5A6Ff4127cDa30944A12B421b9C2", "0xbf924c7081951a52c54836CB05637D8c4C77502d"]) };
+  return { Claims: m.contract("Claims", ["0xEFb36B2D443C5A6Ff4127cDa30944A12B421b9C2", "0x04Ec0582e2700Db583e3BCb9b913D181Ac2D68A8"]) };
 });
 
 export default Module;
