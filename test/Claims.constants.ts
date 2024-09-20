dfeafcaa9d90bbde2f93de2872ae817e4ab47678
xh3b4sd <xh3b4sd@gmail.com>
2024-09-20 11:43:27 +0200

    v0.3.0

--- test/Claims.constants.ts
+++ test/Claims.constants.ts
@@ -32,7 +32,7 @@ describe("Claims", function () {
     it("should expose VERSION constant", async function () {
       const { Claims } = await loadFixture(Deploy);
 
-      expect(await Claims.VERSION()).to.equal("v0.2.0");
+      expect(await Claims.VERSION()).to.equal("v0.3.0");
     });
   });
 });
