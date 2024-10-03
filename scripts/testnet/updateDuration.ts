bddb399cd669be7d0e87dc99b324649467655d89
xh3b4sd <xh3b4sd@gmail.com>
2024-10-03 19:59:58 +0200

    update readme (#84)

--- scripts/testnet/updateDuration.ts
+++ scripts/testnet/updateDuration.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
 
 //
-//     npx hardhat run ./scripts/production/updateDuration.ts --network base-sepolia
+//     npx hardhat run ./scripts/testnet/updateDuration.ts --network base-sepolia
 //
 const main = async () => {
   const Claims = await ethers.getContractAt("Claims", "0x537cE8e9F4Cce5a1D8033B63f274187157a966b3");
