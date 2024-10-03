963bbe62e31b431b9ba8889e0358889415b43c90
xh3b4sd <xh3b4sd@gmail.com>
2024-10-03 20:23:22 +0200

    add testnet deploy scripts (#86)

---
+++ ignition/modules/testnet/UVX.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+//
+//     npx hardhat ignition deploy ./ignition/modules/testnet/UVX.ts --network base-sepolia
+//
+const Module = buildModule("UVX", (m) => {
+  return { UVX: m.contract("UVX", ["0xEFb36B2D443C5A6Ff4127cDa30944A12B421b9C2", "0x7FC9a5730381DdF44C7D762d82A4aabC90fAE786"]) };
+});
+
+export default Module;
