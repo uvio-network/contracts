cac6d88dd4242da452042dde6d99212a0decb7a0
xh3b4sd <xh3b4sd@gmail.com>
2024-10-03 20:07:01 +0200

    v0.5.0

--- test/UVX.constants.ts
+++ test/UVX.constants.ts
@@ -32,7 +32,7 @@ describe("UVX", function () {
     it("should expose VERSION constant", async function () {
       const { UVX } = await loadFixture(Deploy);
 
-      expect(await UVX.VERSION()).to.equal("v0.4.0");
+      expect(await UVX.VERSION()).to.equal("v0.5.0");
     });
   });
 });
